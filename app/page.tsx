e1fdb5e6a6ef9fe13a3cacb53a56718b5749aa57
Evandro Viegas <evandroviegas.com@gmail.com>
2024-12-16 10:50:43 +0000

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import MainContent from "@/components/main-content";
-import MusicPlayer from "@/components/music-player";
-import Navbar from "@/components/navbar";
-
 export default function Home() {
   return (
-    <div className="flex flex-col  gap-3 p-4 max-h-screen h-screen overflow-hidden">
-      <Navbar />
-      <MainContent />
-      <MusicPlayer />
-    </div>
+  <div>
+
+  </div>
   );
 }
--- components/navbar/Logo.tsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-import React from "react";
-
-export default function Logo() {
-  return (
-    <Image src={'/logo.png'} width={32} height={32} alt="Logo" />
-  );
-}
